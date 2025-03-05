266a94cdbc23379bdb032332e4e68301a759b993
sarvesh090901 <sarveshk@genintel.in>
2025-03-05 21:24:27 +0530

    updated airflow_job.py dev

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -11,7 +11,7 @@ default_args = {
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2025, 3, 15),
+    'start_date': datetime(2025, 3, 5),
 }
 
 # Define the DAG
@@ -42,7 +42,7 @@ with DAG(
         task_id="check_file_arrival",
         bucket=gcs_bucket,
         #my_airflow_projects/my_airflow_project_03/source-dev
-        object=f"gs://{gcs_bucket}/my_airflow_project_03/source-{env}/flight_booking.csv",  # Full file path in GCS
+        object=f"my_airflow_projects/my_airflow_project_03/source-{env}/flight_booking.csv",  # Full file path in GCS
         google_cloud_conn_id="google_cloud_default",  # GCP connection
         timeout=300,  # Timeout in seconds
         poke_interval=30,  # Time between checks
