a48c1ec6b365a0387979780e164317a182be6f9f
sarvesh090901 <sarveshk@genintel.in>
2025-03-05 20:01:02 +0530

    changes in code dev

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -24,8 +24,8 @@ with DAG(
 
     # Fetch environment variables
     env = Variable.get("env", default_var="dev")
-    gcs_bucket = Variable.get("gcs_bucket", default_var="airflow-projetcs-gds")
-    bq_project = Variable.get("bq_project", default_var="psyched-service-442305-q1")
+    gcs_bucket = Variable.get("gcs_bucket", default_var="my_airflow_projects")
+    bq_project = Variable.get("bq_project", default_var="turnkey-channel-358109")
     bq_dataset = Variable.get("bq_dataset", default_var=f"flight_data_{env}")
     tables = Variable.get("tables", deserialize_json=True)
 
